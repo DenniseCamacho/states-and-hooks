d3557a6d9aa5037cc9e4fe82e444b51e4fa1d40f
Dennise Camacho <dennise.e.camacho@gmail.com>
2025-08-24 15:16:34 -0500

    adding components back into main file.

--- src/App.js
+++ src/App.js
@@ -7,8 +7,8 @@ function App() {
   return (
     <div className="App">
       <h1>Lab Tasks</h1>
-      {/* <Counter />*/}
-      {/* <UserInput />*/}
+      <Counter />
+      <UserInput />
       <TodoList />
     </div>
   );
