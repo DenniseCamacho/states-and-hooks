6c6961b9f13ba51e7434f4f5ea4aa94a81f3eb7a
Dennise Camacho <dennise.e.camacho@gmail.com>
2025-08-24 14:47:46 -0500

    first push.

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from "react";
+import Counter from "./components/Counter";
+import UserInput from "./components/UserInput";
+import TodoList from "./components/TodoList";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Lab Tasks</h1>
+      {/* <Counter />*/}
+      {/* <UserInput />*/}
+      <TodoList />
+    </div>
+  );
+}
+
+export default App;
